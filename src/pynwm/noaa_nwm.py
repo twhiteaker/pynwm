47e93f2f725f073ca3cbde3b143a79b4d5144ec7
Tim Whiteaker <whiteaker@utexas.edu>
2016-09-28 11:27:58 -0500

    now can list files at noaa ftp

--- src/pynwm/noaa_nwm.py
+++ src/pynwm/noaa_nwm.py
@@ -17,9 +17,102 @@ import urllib
 
 _ftp_url = 'ftpprd.ncep.noaa.gov'
 _root_folder = '/pub/data/nccf/com/nwm/prod/'
+_long_range_products = ['long_range_mem1', 'long_range_mem2',
+                        'long_range_mem3', 'long_range_mem4']
+_all_products = (['analysis_assim', 'short_range', 'medium_range'] +
+                 _long_range_products)
+
+
+def get_datefolders():
+    """Returns a list of available folders from NOAA's FTP server.
+
+    Returns a list of available folders (named by forecast simulation date)
+    from NOAA's FTP server.
+
+    Returns:
+        List of folder names, e.g., ['nwm.20160927', 'nwm.20160928']
+    """
+
+    ftp = FTP(_ftp_url)
+    ftp.login()
+    ftp.cwd(_root_folder)
+    return sorted(ftp.nlst())
+
+
+def list_files(datefolders=None, products=None):
+    """Returns model result filenames for the given dates and products.
+
+    Returns model result filenames for the given forecast dates and products
+    from NOAA's FTP server.  Only 'channel' filenames are returned.
+
+    Args:
+        datefolders: (Optional) List of datefolders or string of a single
+            date folder to crawl, e.g., ['nwm.20160927', 'nwm.20160928'].
+            Use get_datefolders() to get a list of available date folders. If
+            not provided, all available date folders are crawled.
+        products: (Optional) List of NWM products or string of a single
+            product to find, e.g., ['short_range', 'medium_range']. If not
+            provided, all products are returned.  If 'long_range' product is
+            provided, all available long range members are returned. Valid
+            values are:
+                analysis_assim
+                short_range
+                medium_range
+                long_range
+                long_range_mem1
+                long_range_mem2
+                long_range_mem3
+                long_range_mem4
+
+    Returns:
+        List of filenames including directory of available files.
+    """
+
+    if not datefolders:
+        datefolders = get_datefolders()
+    elif type(datefolders) is str:
+        datefolders = [datefolders]
+    datefolders = sorted({f.lower() for f in datefolders})
+    if not products:
+        products = _all_products
+    elif type(products) is str:
+        products = [products.lower()]
+    else:
+        products = {p.lower() for p in products}
+    if 'long_range' in products:
+        products.remove('long_range')
+        products |= set(_long_range_products)
+    products = [p for p in products if p in _all_products]
+
+    ftp = FTP(_ftp_url)
+    ftp.login()
+    ftp.cwd(_root_folder)
+    all_files = []
+    for datefolder in datefolders:
+        for product in products:
+            folder = '{0}/{1}/{2}'.format(_root_folder, datefolder, product)
+            try:
+                ftp.cwd(folder)
+                files = ['{0}/{1}'.format(folder, f) for f in ftp.nlst()
+                         if 'channel' in f]
+                all_files.extend(files)
+            except Exception as ex:
+                print str(ex)
+            break
+    return all_files
 
 
 def get_latest_analysis_filename():
+    """Gets name of latest analysis and assimilation file from NOAA.
+
+    Gets name of latest analysis and assimilation file from NOAA. Filename
+    includes FTP folder, e.g.,
+    /pub/data/nccf/com/nwm/prod/nwm.20160928/analysis_assim/nwm.t15z.analysis_assim.channel_rt.tm00.conus.nc.gz
+
+    Returns:
+        Filename, including FTP directory, of the file.
+    """
+
     ftp = FTP(_ftp_url)
     ftp.login()
     ftp.cwd(_root_folder)
